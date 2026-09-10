903590583bc45967a3ac7e348208f5d802fa4cc7
Doeydonuts <jonanclay@gmail.com>
2026-09-10 17:35:43 +1000

    Add firebase config

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1fkEvMaiYpd62qXzmwF8bgESzKC7RwvU",
+  authDomain: "handballelo.firebaseapp.com",
+  projectId: "handballelo",
+  storageBucket: "handballelo.firebasestorage.app",
+  messagingSenderId: "974918107704",
+  appId: "1:974918107704:web:cf71dbf07136953c715ccd",
+  measurementId: "G-VVND9LRD4Q"
+};
+
+export const ADMIN_UIDS = [
+  "PASTE_YOUR_UID_HERE"
+];
