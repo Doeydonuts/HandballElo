1f08f8002780d5f2828517e3e69043cb8cb5c220
Doeydonuts <jonanclay@gmail.com>
2026-09-11 10:20:25 +1000

    Added admin

--- firebase-config.js
+++ firebase-config.js
@@ -10,5 +10,6 @@ export const firebaseConfig = {
 
 export const ADMIN_UIDS = [
   "fFEgf8e6ylZxVqORoGr0K5W2Yme2",
-  "YkbsW3f1cmSd5DopijW7r1AicXv2"
+  "YkbsW3f1cmSd5DopijW7r1AicXv2",
+  "XLLutQwNuURO4ImmN20Kyd4EORJ2"
 ];
